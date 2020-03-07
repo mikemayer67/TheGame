dd398c167c13d08a23ed15d710aca0897d6f33c2
Michael A Mayer <mikemayer67@vmwishes.com>
2020-03-07 18:41:53 -0500

    Checkpoint before adding SO fix to dictation in textfield

---
+++ TheGame/Support Classes/DictationAwareTextView.swift
@@ -0,0 +1,21 @@
+//
+//  DictationAwareTextView.swift
+//  TheGame
+//
+//  Created by Mike Mayer on 3/6/20.
+//  Copyright © 2020 VMWishes. All rights reserved.
+//
+
+import UIKit
+
+class DictationAwareTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
