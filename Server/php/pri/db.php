d91e0b80252ebd60ff588a021738e81dc2b9ee97
Michael A Mayer <mikemayer67@vmwishes.com>
2020-05-22 14:04:29 -0400

    Cleanup

--- Server/php/pri/db.php
+++ Server/php/pri/db.php
@@ -1,23 +1,5 @@
 <?php
 
-class DB
-{
-  const USERID      = 'userid';
-  const FBID        = 'fbid';
-  const USERKEY     = 'userkey';
-  const USERNAME    = 'username';
-  const PASSWORD    = 'password';
-  const ALIAS       = 'alias';
-  const EMAIL       = 'email';
-  const EMAIL_VAL   = 'email_validation';
-  const LASTLOSS    = 'last_loss';
-  const VALIDATED   = 'Y';
-  const UPDATED     = 'updated';
-  const SCOPE       = 'scope';
-  const NOTIFY      = 'notify';
-}
-
-
 class TGDB {
   public  $db = null;
 
